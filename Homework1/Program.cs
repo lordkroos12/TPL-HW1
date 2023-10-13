727343cf96340aed8cd998aa0fad8a5e99383155
lordkroos12 <joroo.23@abv.bg>
2023-10-13 13:01:04 +0300

    Add project files.

---
+++ Homework1/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Homework1
+{
+    //Average elapsed time of the multithread program: 273,8ms 
+    //Average elapsed time of the non-multithread program: 303,6ms
+
+    internal class Program
+    {
+        static string text = File.ReadAllText("book.txt");
+        public static int wordCount = 0;
+        public static void AverageWordLenght()
+        {
+            var sb = new StringBuilder();
+            foreach (var letter in text)
+            {
+                if (Char.IsLetter(letter))
+                {
+                    sb.Append(letter);
+                }
+
+            }
+            Console.WriteLine($"The average word length is: {sb.Length / wordCount}");
+
+
+        }
+        public static void RemoveSpecialCharacters()
+        {
+            text = text.Replace("\n", " ");
+            var sb = new StringBuilder();
+            foreach (char c in text)
+            {
+                if ((c >= 'А' && c <= 'Я') || (c >= 'а' && c <= 'я') || c == ' ')
+                {
+                    sb.Append(c);
+                }
+            }
+            text = sb.ToString();
+        }
+
+        public static void WordCount()
+        {
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach (var item in words)
+            {
+                if (item.Length >= 3)
+                {
+                    count++;
+                }
+            }
+            wordCount = count;
+            Console.WriteLine($"The word count is: {count}");
+        }
+        public static void ShortestWord()
+        {
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string shortestWord = words[0];
+            foreach (var item in words)
+            {
+                if (item.Length >= 3 && item.Length < shortestWord.Length)
+                {
+                    shortestWord = item;
+                }
+            }
+            Console.WriteLine($"The shortest word is: {shortestWord}");
+        }
+        public static void LongestWord()
+        {
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string longestWord = words[0];
+            foreach (var item in words)
+            {
+                if (item.Length >= 3 && item.Length > longestWord.Length)
+                {
+                    longestWord = item;
+                }
+            }
+            Console.WriteLine($"The longest word is: {longestWord}");
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch sp = new Stopwatch();
+            sp.Start();
+            Thread t1 = new Thread(() => RemoveSpecialCharacters());
+            t1.Start();
+            Thread t2 = new Thread(() => WordCount());
+            t1.Join();
+            t2.Start();
+            Thread t3 = new Thread(() => ShortestWord());
+            t3.Start();
+            Thread t4 = new Thread(() => LongestWord());
+            t4.Start();
+            Thread t5 = new Thread(() => AverageWordLenght());
+            t2.Join();
+            t5.Start();
+            Thread t6 = new Thread(() => LeastCommonWord());
+            t6.Start();
+            Thread t7 = new Thread(() => MostCommonWord());
+            t7.Start();
+            t3.Join();
+            t4.Join();
+            t5.Join();
+            t6.Join();
+            t7.Join();
+            sp.Stop();
+            Console.WriteLine($"Elapsed time: {sp.ElapsedMilliseconds} ms");
+        }
+
+        private static void MostCommonWord()
+        {
+            string[] mwords = new string[5];
+            int myNumber = int.MinValue;
+            string myWord = "";
+            text = text.ToLower();
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> wordcount = new Dictionary<string, int>();
+            foreach (string item in words)
+            {
+                if (item.Length >= 3)
+                {
+                    if (!wordcount.ContainsKey(item))
+                    {
+                        wordcount.Add(item, 1);
+                    }
+                    else
+                    {
+                        wordcount[item]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                foreach (var item in wordcount)
+                {
+                    if (item.Value > myNumber)
+                    {
+                        myWord = item.Key;
+                        myNumber = item.Value;
+                    }
+                }
+                myNumber = int.MinValue;
+                mwords[i] = myWord;
+                wordcount.Remove(myWord);
+            }
+            Console.WriteLine($"The five most common words are: {String.Join(", ", mwords)}");
+
+        }
+
+        private static void LeastCommonWord()
+        {
+            string[] lwords = new string[5];
+            int myNumber = int.MaxValue;
+            string myWord = "";
+            text = text.ToLower();
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> wordcount = new Dictionary<string, int>();
+            foreach (string item in words)
+            {
+                if (item.Length >= 3)
+                {
+                    if (!wordcount.ContainsKey(item))
+                    {
+                        wordcount.Add(item, 1);
+                    }
+                    else
+                    {
+                        wordcount[item]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                foreach (var item in wordcount)
+                {
+                    if (item.Value < myNumber)
+                    {
+                        myWord = item.Key;
+                        myNumber = item.Value;
+                    }
+                }
+                myNumber = int.MaxValue;
+                lwords[i] = myWord;
+                wordcount.Remove(myWord);
+            }
+            Console.WriteLine($"The five least common words are: {String.Join(", ", lwords)}");
+        }
+
+
+    }
+
+}
+
+
+
+
